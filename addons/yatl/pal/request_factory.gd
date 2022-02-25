faec03c24d053258c702ad0c360e8cda342f0b0f
Aiden Storey <git@storey.dev>
2022-02-24 22:18:02 -0500

    Add initial http request factory and response

--- addons/yatl/pal/request_factory.gd
+++ addons/yatl/pal/request_factory.gd
@@ -3,4 +3,44 @@ extends Node
 
 # Private imports
 
-const __HTTPResponse: Resource = preload('../shared/http_response.gd')
+const __HTTPResponse: Resource = preload("../shared/http_response.gd")
+
+
+# Public methods
+
+func request(
+	url: String,
+	headers: Dictionary = {},
+	use_ssl: bool = true,
+	method: int = 0,
+	data: String = ""
+) -> __HTTPResponse:
+	var request: HTTPRequest = HTTPRequest.new()
+	request.use_threads = true # TODO: Make this a setting
+
+	add_child(request)
+
+	var response: __HTTPResponse = __HTTPResponse.new(request)
+
+	var formatted_headers: PoolStringArray = PoolStringArray()
+
+	for header in headers:
+		formatted_headers.append(
+			"%s: %s" % [header, headers[header]]
+		)
+
+	var error: int = request.request(
+		url,
+		formatted_headers,
+		use_ssl,
+		method,
+		data
+	)
+
+	if error != OK:
+		push_error("An error occured while making the HTTP request.")
+
+		response.__request_error(error)
+
+	return response
+
--- addons/yatl/shared/http_response.gd
+++ addons/yatl/shared/http_response.gd
@@ -1,2 +1,65 @@
 extends Resource
 
+# Public signals
+
+signal completed(response)
+
+
+# Private variables
+
+var request: HTTPRequest = null
+
+var error: int setget __noop_set # Error
+var result: int setget __noop_set # HTTPRequest Result
+var response_code: int setget __noop_set
+var headers: Dictionary setget __noop_set
+var body: PoolByteArray setget __noop_set
+
+
+# Lifecycle methods
+
+func _init(
+	request: HTTPRequest
+) -> void:
+	self.request = request
+
+	request.connect("request_completed", self, "__request_completed")
+
+
+# Private methods
+
+func __noop_set(_value) -> void:
+	pass
+
+
+func __request_completed(
+	p_result: int,
+	p_response_code: int,
+	p_headers: PoolStringArray,
+	p_body: PoolByteArray
+) -> void:
+	result = p_result
+	response_code = p_response_code
+	headers = {}
+
+	for header in p_headers:
+		var parts: Array = header.split(":")
+
+		var name: String = parts[0].trim_prefix(" ").trim_suffix(" ")
+		var value: String = parts[1].trim_prefix(" ").trim_suffix(" ")
+
+		headers[name] = value
+
+	body = p_body
+
+	emit_signal("completed", self)
+
+
+func __request_error(
+	p_error: int
+) -> void:
+	error = error
+
+	yield(request.get_tree(), "idle_frame")
+
+	emit_signal("completed", self)
