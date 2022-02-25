920c7d8d28a8f14253da7a730d0258167011435f
velopman <74576289+velopman@users.noreply.github.com>
2022-02-24 22:18:17 -0500

    Add testing scene

---
+++ source/main.gd
@@ -0,0 +1,14 @@
+extends Control
+
+const HTTPRequestFactory = preload("res://addons/yatl/pal/request_factory.gd")
+
+
+func _ready() -> void:
+	var request_factory: HTTPRequestFactory = HTTPRequestFactory.new()
+	add_child(request_factory)
+
+	var a = yield(request_factory.request("https://httpstat.us/200"), "completed")
+	print(a.response_code)
+
+	var b = yield(request_factory.request("https://httpstat.us/400"), "completed")
+	print(b.response_code)
