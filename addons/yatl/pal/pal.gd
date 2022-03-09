e70cdaab4b1434b2aa44a7790906fe0b6147c368
velopman <74576289+velopman@users.noreply.github.com>
2022-03-08 23:01:39 -0500

    Fix dependencies

--- addons/yatl/pal/pal.gd
+++ addons/yatl/pal/pal.gd
@@ -3,5 +3,5 @@ extends Node
 
 # Private imports
 
-const __RequestFactory: Resource = preload("./request_factory.gd")
-const __WebsocketFactory: Resource = preload('./websocket_factory.gd')
+const __RequestFactory: Resource = preload("./factories/request_factory.gd")
+const __WebsocketFactory: Resource = preload('./factories/websocket_factory.gd')
--- source/main.gd
+++ source/main.gd
@@ -1,6 +1,7 @@
 extends Control
 
-const RequestFactory = preload("res://addons/yatl/pal/request_factory.gd")
+const RequestFactory = preload("res://addons/yatl/pal/factories/request_factory.gd")
+const WebSocketFactory = preload("res://addons/yatl/pal/factories/web_socket_factory.gd")
 
 
 func _ready() -> void:
@@ -12,3 +13,9 @@ func _ready() -> void:
 
 	var b = yield(request_factory.request("https://httpstat.us/400"), "completed")
 	print(b.response_code)
+
+	var web_socket_factory: WebSocketFactory = WebSocketFactory.new()
+	add_child(web_socket_factory)
+
+	var c = yield(web_socket_factory.establish_connection("ws://test.we:8000"), "completed")
+	print(c.connected)
