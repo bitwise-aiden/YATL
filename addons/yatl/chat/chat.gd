ed82a77eb16b4dd379b9ab4450d54668e09940d1
Lera Elvoé <eturbo38@gmail.com>
2022-03-19 21:26:20 +0300

    port chat functionality

--- addons/yatl/chat/chat.gd
+++ addons/yatl/chat/chat.gd
@@ -1,163 +1,170 @@
-tool
-extends Node
-
-
 # Private imports
 
 const __PAL: Resource = preload("../pal/pal.gd")
+const __WebSocketConnection: Resource = __PAL.WebSocketConnection
 
 
-#######
-#
-# TODO: Update this code for new PAL abstraction
-#
-#######
+# Public imports
 
-
-#Custom class preloads
 const ChatMessage = preload("./chat_message.gd")
 
 
-#URL constants
+#Signals
+
+signal connected()
+signal login(success) # success: bool
+signal disconnected()
+signal connection_failed()
+signal channel_joined(channel) # channel: String
+signal channel_left(channel) # channel: String
+signal message_received(chat_message) # chat_message: ChatMessage
+signal whisper_received(chat_message) # chat_message: ChatMessage
+
+
+# URL constants
+
 const CHAT_WSS_URL: String = "wss://irc-ws.chat.twitch.tv:443"
 
 
-#Signals
-signal chat_connected(yatl_instance)
-signal chat_login(yatl_instance, success) # success: bool
-signal chat_disconnected(yatl_instance, clean) # clean: bool
-signal chat_connection_failed(yatl_instance)
-signal chat_joined(yatl_instance, channel) # channel: String
-signal chat_message_received(yatl_instance, chat_message) # chat_message: ChatMessage
-signal chat_whisper_received(yatl_instance, chat_message) # chat_message: ChatMessage
-signal chat_raw_data_received(yatl_instance, raw_data) # raw_data: String
-signal chat_ping_received(yatl_instance)
+# Public variables
+
+var reconnect: bool = false # Whether or not to try and reconnect automatically
+var message_queue_timeout: float = 0.33 # how long to wait after sending each message
 
 
-#Private variables
-var __socket: WebSocketClient = WebSocketClient.new()
-var __channel_list: Array = []
+# Private variables
 
+var __pal: __PAL
+
+var __client: WebSocketClient
+var __socket_connection: __WebSocketConnection
+
+var __current_channel: String
 var __username_pattern: String = ":([\\w]+)!"
 var __username_regex = RegEx.new()
 
+var __message_queue: Array = []
+var __time_elapsed: float = 0.0
 
-#Lifecycle methods
-func _init() -> void:
-	__socket.verify_ssl = true
 
+# Lifecycle methods
+
+func _init(pal: __PAL) -> void:
+	__pal = pal
+	
 	__username_regex.compile(__username_pattern)
 
-	__socket.connect("connection_closed", self, "__socket_connection_closed")
-	__socket.connect("connection_established", self, "__socket_connection_established")
-	__socket.connect("connection_error", self, "__socket_connection_error")
-	__socket.connect("data_received", self, "__socket_data_received")
-	__socket.connect("server_close_request", self, "__socket_server_close_request")
+
+func _process(delta: float) -> void:
+	if !__socket_connection || !__socket_connection.connected:
+		return
+	
+	if !__message_queue.empty() && __time_elapsed >= message_queue_timeout:
+		__client_put_message(__message_queue.pop_front())
+		__time_elapsed = 0
+	else:
+		__time_elapsed += delta
 
 
-func _process(_delta: float) -> void:
-	if __socket.get_connection_status() != __socket.CONNECTION_DISCONNECTED:
-		__socket.poll()
+# Public methods
 
-
-#Public methods - chat
 func chat_connect() -> void:
-	print("chat connect called")
-	if __socket.connect_to_url(CHAT_WSS_URL) != OK:
-		emit_signal("chat_connection_failed", self)
+	if __socket_connection || __socket_connection.connected:
+		return
+	
+	__socket_connection = __pal.establish_connection(CHAT_WSS_URL)
+	yield(__socket_connection, "completed")
+	if __socket_connection.connected:
+		emit_signal("connected")
+		__socket_connection.connect("disconnected", self, "__on_socket_connection_disconnected")
+		__client = __socket_connection.client
+	else:
+		emit_signal("connection_failed")
 
 
 func chat_disconnect() -> void:
-	if !(__socket.get_peer(1).is_connected_to_host()):
+	if !__socket_connection || !__socket_connection.connected:
 		return
+	
+	__socket_connection.client.disconnect_from_host() # TODO: velop expose disconnect_from_host() to WebsocketConnection
+	yield(__socket_connection, "disconnected")
+	__client = null
+	__socket_connection = null
 
-	__socket.disconnect_from_host()
 
-
-func chat_auth(nick: String, token: String) -> void:
-	__socket.get_peer(1).set_write_mode(WebSocketPeer.WRITE_MODE_TEXT)
-	__socket_send("PASS " + ("" if token.begins_with("oauth:") else "oauth:") + token)
-	__socket_send("NICK " + nick.to_lower())
-	__socket_send("CAP REQ :twitch.tv/commands twitch.tv/tags twitch.tv/membership")
+func authenticate(nick: String, token: String) -> void:
+	if !__socket_connection || !__socket_connection.connected:
+		return
+	
+	__client.get_peer(1).set_write_mode(WebSocketPeer.WRITE_MODE_TEXT)
+	__client_put_message("PASS " + ("" if token.begins_with("oauth:") else "oauth:") + token)
+	__client_put_message("NICK " + nick.to_lower())
+	__client_put_message("CAP REQ :twitch.tv/commands twitch.tv/tags twitch.tv/membership")
 
 
 func join_channel(channel: String) -> void:
-	__socket_send("JOIN " + ("" if channel.begins_with("#") else "#") + channel.to_lower())
-#	__current_channel = channel
+	__client_put_message("JOIN " + ("" if channel.begins_with("#") else "#") + channel.to_lower().strip_edges())
 
 
-func leave_channel(channel: String = "") -> void:
-#	if __current_channel == "":
-#		return
-
-	__socket_send("PART " + ("" if channel.begins_with("#") else "#") + channel.to_lower())
-#	__current_channel = ""
-
-
-func send_message(message: String) -> void: #TODO: message queue
-#	if __current_channel == "":
-#		return
-	pass
-#	__socket_send("PRIVMSG " + ("" if __current_channel.begins_with("#") else "#") + __current_channel + " :" + message)
+func leave_current_channel() -> void:
+	if __current_channel.empty():
+		return
+	
+	__client_put_message("PART " + __current_channel)
 
 
-#Private methods
-func __socket_connection_closed(clean: bool) -> void:
-	print("disconnected, clean ", clean)
-	emit_signal("chat_disconnected", self, clean)
+func send_message(message: String) -> void:
+	__message_queue.append("PRIVMSG #" + __current_channel + " :" + message)
 
 
-func __socket_connection_established(protocol: String) -> void:
-	print("connection established with protocol ", protocol)
-	emit_signal("chat_connected", self)
+# Private methods
+
+func __on_socket_connection_disconnected():
+	emit_signal("disconnected")
 
 
-func __socket_connection_error() -> void:
-	print("connection error")
-	emit_signal("chat_connection_failed", self)
-
-
-func __socket_data_received() -> void:
-	var data: PoolByteArray = __socket.get_peer(1).get_packet()
-	var data_string: String = data.get_string_from_utf8().strip_edges(false)
-	emit_signal("chat_raw_data_received", self, data_string)
-	var messages: PoolStringArray = data_string.split("\r\n")
-#	print("> ", data_string)
+func __on_socket_connection_data_received(data: String) -> void:
+	data = data.strip_edges(false)
+	var messages: PoolStringArray = data.split("\r\n")
 	var tags: Dictionary = {}
 	for message in messages:
 		if message.begins_with("@"):
 			var new_message: PoolStringArray = message.split(" ", false, 1)
 			message = new_message[1]
-			for t in new_message[0].split(";"):
-				var kv: PoolStringArray = t.split("=")
+			for tag in new_message[0].split(";"):
+				var kv: PoolStringArray = tag.split("=")
 				tags[kv[0]] = kv[1]
 
-		__chat_message(message, tags)
+		__handle_chat_message(message, tags)
 
 
-func __socket_server_close_request(code: int, reason: String) -> void:
-	print("server close request, code ", code, "; reason ", reason)
-
-
-func __socket_send(message: String) -> void:
-	__socket.get_peer(1).put_packet(message.to_utf8())
-
-
-func __chat_message(message: String, tags: Dictionary) -> void:
-	print("> ", message)
-	if(message == ":tmi.twitch.tv NOTICE * :Login authentication failed"):
-		emit_signal("chat_login", self, false)
+func __handle_chat_message(message: String, tags: Dictionary) -> void:
+	if message == ":tmi.twitch.tv NOTICE * :Login authentication failed":
+		emit_signal("login", false)
 		return
-
-	if(message == "PING :tmi.twitch.tv"):
-		emit_signal("chat_ping_received", self)
-		__socket_send("PONG :tmi.twitch.tv")
+	
+	if message == "PING :tmi.twitch.tv":
+		__client_put_message("PONG :tmi.twitch.tv")
 		return
-
+	
 	var split: PoolStringArray = message.split(" ", true, 3)
 	match split[1]:
 		"001":
-			emit_signal("chat_login", self, true)
+			emit_signal("login", true)
 		"PRIVMSG":
 			var chat_message: ChatMessage = ChatMessage.new(__username_regex.search(split[0]).get_string(1), split[3].right(1), split[2], tags)
-			emit_signal("chat_message_received", self, chat_message)
+			emit_signal("message_received", chat_message)
+		"JOIN":
+			__current_channel = split[2]
+			emit_signal("channel_joined", __current_channel)
+		"PART":
+			emit_signal("channel_left", __current_channel)
+			__current_channel = split[2]
+		#TODO: WHISPER, NOTICE, RECONNECT
+
+
+func __client_put_message(message: String) -> void:
+	if !__client:
+		return
+	
+	__client.get_peer(1).put_packet(message.to_utf8())
