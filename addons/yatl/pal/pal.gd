d6b77f3c9ed88a3f77cf6af5d947b7f792fe0112
Aiden Storey <git@storey.dev>
2022-03-19 12:10:33 -0400

    Update pal to expose web socket and http requests

--- addons/yatl/pal/pal.gd
+++ addons/yatl/pal/pal.gd
@@ -1,7 +1,48 @@
 extends Node
 
+# Public imports
+
+const HTTPResponse: Resource = preload("../shared/http_response.gd")
+const WebSocketConnection: Resource = preload("../shared/web_socket_connection.gd")
+
 
 # Private imports
 
 const __RequestFactory: Resource = preload("./factories/request_factory.gd")
 const __WebSocketFactory: Resource = preload('./factories/web_socket_factory.gd')
+
+
+# Private variables
+
+var __request_factory: __RequestFactory = __RequestFactory.new()
+var __web_socket_factory: __WebSocketFactory = __WebSocketFactory.new()
+
+# Lifecylce methods
+
+func _ready() -> void:
+	add_child(__request_factory)
+	add_child(__web_socket_factory)
+
+
+# Public methods
+
+func establish_connection(
+	_url: String
+) -> WebSocketConnection:
+	return __web_socket_factory.establish_connection(_url)
+
+
+func request(
+	_url: String,
+	_headers: Dictionary = {},
+	_use_ssl: bool = true,
+	_method: int = 0,
+	_data: String = ""
+) -> HTTPResponse:
+	return __request_factory.request(
+		_url,
+		_headers,
+		_use_ssl,
+		_method,
+		_data
+	)
