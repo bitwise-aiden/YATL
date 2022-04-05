459bcb760e9b01c632ca9b06f39e70fc0f4a694a
Aiden Storey <git@storey.dev>
2022-04-04 23:59:30 -0400

    Update http request and response objects

--- addons/yatl/pal/types/http_request_state.gd
+++ addons/yatl/pal/types/http_request_state.gd
@@ -5,11 +5,12 @@ const __HTTPResponse = preload("./http_response.gd")
 
 # Public signals
 
-signal completed(result, response)
+signal completed(response)
 
 
 # Public variables
 
+var completed: bool setget __noop_set
 var error: int setget __noop_set
 var request: HTTPRequest setget __noop_set
 var response: __HTTPResponse setget __noop_set
@@ -18,10 +19,31 @@ var response: __HTTPResponse setget __noop_set
 # Lifecycle methods
 
 func _init(
-	_request: HTTPRequest
+	_request: HTTPRequest,
+	_error: int = OK
 ) -> void:
-	request = _request
-	request.connect("request_completed", self, "__completed")
+	error = _error
+
+	if error == OK:
+		request = _request
+		request.connect("request_completed", self, "__completed")
+	else:
+		yield(_request.get_tree(), "idle_frame")
+
+		emit_signal("completed", response)
+
+
+# Public methods
+
+func cancel() -> void:
+	if request == null:
+		return
+
+	request.cancel_request()
+
+
+func is_valid() -> bool:
+	return error != OK
 
 
 # Private methods
@@ -41,7 +63,9 @@ func __completed(
 	else:
 		pass # TODO: Determine what to do here if the result isn't OK
 
-	emit_signal("completed", _result, response)
+	request = null
+
+	emit_signal("completed", response)
 
 
 func __noop_set(
@@ -58,8 +82,8 @@ func __parse_headers(
 	for header in _headers:
 		var parts: Array = header.split(":")
 
-		var name: String = parts[0].trim_prefix(" ").trim_suffix(" ")
-		var value: String = parts[1].trim_prefix(" ").trim_suffix(" ")
+		var name: String = parts[0].strip_edges()
+		var value: String = parts[1].strip_edges()
 
 		headers[name] = value
 
