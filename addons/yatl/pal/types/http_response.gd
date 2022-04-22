5237bad8a6b4f762e5bc29c19e4e9e0431a82404
velopman <74576289+velopman@users.noreply.github.com>
2022-04-21 21:13:23 -0400

    Add result to response object

--- addons/yatl/pal/types/http_response.gd
+++ addons/yatl/pal/types/http_response.gd
@@ -1,24 +1,33 @@
 # Public variables
 
-var status_code: int setget __noop_set
-var headers: Dictionary setget __noop_set
 var body: String setget __noop_set
+var headers: Dictionary setget __noop_set
 var json setget __noop_set # Variant
+var result: int setget __noop_set
+var status_code: int setget __noop_set
 
 
 # Lifecycle methods
 
 func _init(
+	_result: int,
 	_status_code: int,
 	_headers: Dictionary,
 	_body: String
 ) -> void:
-	status_code = _status_code
 	headers = _headers
 	body = _body
+	result = _result
+	status_code = _status_code
 
 	if headers.get("content-type", "") == "application/json":
 		json = parse_json(body)
+
+
+# Public methods
+
+func is_valid() -> bool:
+	return result != HTTPRequest.RESULT_SUCCESS
 
 
 # Private methods
